8a7f31a1e8cab9b95fc0174b7a4fd963e0e2df74
MatthewACoelho <49939157+MatthewACoelho@users.noreply.github.com>
2022-01-05 14:12:31 +0000

    Add files via upload

--- be-view_app/app.R
+++ be-view_app/app.R
@@ -10,8 +10,8 @@ library(digest)
 library(plotly)
 
 data <- as_tibble(read.csv(file = "input.csv", header = TRUE)) %>%
-    select(!X) %>%
-    filter(is.na(L2FC_IFNg_Control_prolif) == FALSE)
+    select(!(c(X, X.1))) %>%
+    filter(is.na(zscore_proliferation) == FALSE)
 
 pos <- position_jitter(seed = 2, width = 0.2, height = 0.2)
 
@@ -33,6 +33,7 @@ ui <- fluidPage(
          )
        ),
     br(),
+    
     # Sidebar with input 
     sidebarLayout(
         sidebarPanel(
@@ -41,12 +42,9 @@ ui <- fluidPage(
         selectInput("screen",
                   "screen", c("proliferation", "FACS")),
         selectInput("editor",
-                    "editor", c("CBE", "ABE")),
-        selectInput("PAM",
-                    "PAM", c("NGG")),
+                    "editor", c("BE3_NGG_JAK1_focused", "BE3_NGG_IFNG_pathway", "BE4max_YE1_NGN", "BE3.9max_NGN", "ABE8e_NGN")),
         hr(),
         downloadButton("downloadData", "download results"),
-        hr(),
         ),
         
         #MAIN
@@ -76,14 +74,13 @@ ui <- fluidPage(
             br(),
             a("Matt Coelho's GitHub", href = "https://github.com/MatthewACoelho/"),
             br(),
-            a("BEstimate GitHub", href = "https://github.com/CansuDincer/BEstimate"),
             hr(),
             h5("License"),
             h6("Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
             The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
             THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."),
             hr(),
-            h6("Shiny app development by Matt Coelho. Analysis by Matt Coelho. We used BEstimate software and VEP for base editing predictions"),
+            h6("Shiny app development by Matt Coelho. Analysis by Matt Coelho. We used VEP for base editing predictions"),
             a("VEP", href = "https://www.ensembl.org/info/docs/tools/vep/index.html"),
          
         )
@@ -99,7 +96,8 @@ server <- function(input, output) {
     
     results <- reactive({
                 data <- data %>% filter(Gene == input$gene) %>%
-                    filter(editor == input$editor)
+                    filter(editor == input$editor) %>%
+                    select(!c(Amino_Acid_Position_simple))
     })
     
     
@@ -118,26 +116,26 @@ server <- function(input, output) {
         
         if (input$screen == "proliferation") {
             plot_ly(data, 
-                    x=~Protein_Position, 
-                    y=~L2FC_IFNg_Control_prolif,
+                    x=~Amino_Acid_Position_simple, 
+                    y=~zscore_proliferation,
                     color=~Consequence,
-                    colors = c("black", "#fabd2f", "#cc241d"),
+                    colors = c("black", "#fabd2f", "#cc241d", "blue"),
                     size = 2,
                     alpha = 0.8,
                     hoverinfo="text", 
-                    text = ~paste0(Protein_Change)
-            ) %>% layout(xaxis=list(title = "amino acid position"), yaxis = list(title = "L2FC"), legend = list(title = list(text = "consequence")))
+                    text = ~paste0(Amino_Acid_Change)
+            ) %>% layout(xaxis=list(title = "amino acid position"), yaxis = list(title = "z-score"), legend = list(title = list(text = "consequence")))
         } else {
             plot_ly(data, 
-                    x=~Protein_Position, 
-                    y=~L2FC_IFNg_Control_FACS,
+                    x=~Amino_Acid_Position_simple, 
+                    y=~zscore_FACS,
                     color=~Consequence,
-                    colors = c("black", "#fabd2f", "#cc241d"),
+                    colors = c("black", "#fabd2f", "#cc241d", "blue"),
                     size = 2,
                     alpha = 0.8,
-                    hoverinfo="text", 
-                    text = ~paste0(Protein_Change)
-            ) %>% layout(xaxis=list(title = "amino acid position"), yaxis = list(title = "L2FC"), legend = list(title = list(text = "consequence")))
+                    hoverinfo="text",
+                    text = ~paste0(Amino_Acid_Change)
+            ) %>% layout(xaxis=list(title = "amino acid position"), yaxis = list(title = "z-score"), legend = list(title = list(text = "consequence")))
         }
         
     })
@@ -156,7 +154,7 @@ server <- function(input, output) {
     # Downloadable txt of data
     output$downloadData <- downloadHandler(
         filename = function() {
-            paste(c(input$gene, "_final.txt"), collapse = "")
+            paste(c(input$gene, "_mutagenesis.txt"), collapse = "")
         },
         content = function(file) {
             write.table(results(), file, sep ="\t")
